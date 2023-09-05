2a9ecbfe1c736aa388703fe0b938e263bc18cc4a
Gustavo Durand <gustavodurandd@gmail.com>
2023-09-05 16:05:46 -0300

    Commit

--- cypress/e2e/Zendata-test-dev/dashboard_dev_test.cy.js
+++ cypress/e2e/Zendata-test-dev/dashboard_dev_test.cy.js
@@ -1,11 +1,10 @@
-beforeEach(function(){
+describe('Dashboard Zendata', function (){
+})
+it('Validar aplicaciones en el menú',()=>{
     cy.visit('https://develop.zendata.dev/login'); // comando
     cy.get('[placeholder="johndoe@zendata.dev"]').type('gustavodurandd@gmail.com').should('be.visible'); //type user)
     cy.get('[placeholder="********"]').type('Gjde2112.{enter}').should('be.visible');//type password and login
-})
-
-it('Validar aplicaciones en el menú',()=>{
-    cy.get('#root > div > div > div > ul > a.MuiButtonBase-root.MuiListItem-root.jss64.-reports.-active.MuiListItem-gutters.MuiListItem-button.active > div.MuiListItemText-root.jss65 > span'); //Icon Zendata principal
+    cy.get('#root > div > div > div > ul > a.MuiButtonBase-root.MuiListItem-root.jss64.-reports.-active.MuiListItem-gutters.MuiListItem-button.active > div.MuiListItemText-root.jss65 > span').should('exist'); //Icon Zendata principal
     cy.get('div > div > div > ul > a:nth-child(1)').should('be.visible');//Website scanner
     cy.get('div > div > div > ul > a:nth-child(2)').should('be.visible');//App Scanner
     cy.get('div > div > div > ul > a:nth-child(3)').should('be.visible');//Security Scanner
@@ -16,9 +15,6 @@ it('Validar aplicaciones en el menú',()=>{
     cy.get('div > div > div > ul > a:nth-child(8)').should('be.visible');//DB Scanner
     cy.get('div > div > div > ul > a:nth-child(9)').should('be.visible');//Privacy Badges
     cy.get('div > div > div > ul > a:nth-child(11)').should('be.visible');//Account
-});
-
-it('Validar contenido dashboard principal',()=>{
     cy.get('[class="jss112 -selected"]').should('be.visible');// Sub menu Reports
     cy.get('div > div> div>div>div>button:nth-child(2)').should('be.visible');//New Scan
     cy.get('div > div> div>div>div>button:nth-child(3)').should('be.visible');//Scheduled Scans
@@ -29,6 +25,3 @@ it('Validar contenido dashboard principal',()=>{
     cy.get('div >div>ul>li').should('be.visible');//User - Logout
     cy.get('#user-menu > div:nth-child(1)').click();//User - Logout
 })
-after(() => {
-
-});
