4037c506cd62c84619242ceb4382051a0a4e5501
Gustavo Durand <gustavodurandd@gmail.com>
2023-09-05 14:48:30 -0300

    commit

---
+++ cypress/e2e/Zendata-test-dev/login_dev_test.cy.js
@@ -0,0 +1,27 @@
+describe('Login Zendata', function (){
+    beforeEach(function(){
+    cy.visit('https://develop.zendata.dev/login');
+  })
+  it('Validar contenido del Login', () => {
+    cy.visit('https://app.zendata.dev/login')
+    cy.get('div > main > div > a').should('be.visible'); // Back to homepage button
+    cy.get('div > main > div > div > a > svg').should('be.visible'); // Zendata icon
+    cy.get('div > main > div > div > h1').should('be.visible'); // Welcome back title
+    cy.get('div > main > div > div > p').should('be.visible'); // Title "Complete the following form to log in"
+    cy.get('a[href="/reset-password"]').should('be.visible');// link forgot password
+    cy.get('a[href="https://www.zendata.dev/pricing"]').should('be.visible'); // Link Sign Up
+    cy.get('main >div  footer > p > a').should('be.visible');// Link Zendata footer
+    cy.get('div > main > div > div button').should('be.visible');// Sign In button
+    cy.get('[class="MuiGrid-root jss6 MuiGrid-item MuiGrid-grid-md-6"]').should('be.visible'); // body right
+  })
+  it('Validar acceso', () => {
+    cy.get('[placeholder="johndoe@zendata.dev"]').type('gustavodurandd@gmail.com').should('be.visible'); //type user)
+    cy.get('[placeholder="********"]').type('Gjde2112.').should('be.visible');//type password and login
+    cy.get('div >  div  > form > button').click();//click on button "Sign In"
+        
+  })
+  after(() => {
+
+  });
+
+})
--- cypress/e2e/Zendata-test-production/dashboard_production_test.cy.js
+++ cypress/e2e/Zendata-test-production/dashboard_production_test.cy.js
@@ -27,7 +27,7 @@ describe('Dashboard Test', function(){
         cy.get('div > div> div>div>div>button:nth-child(4)').should('be.visible');//Single-run Scans
         cy.get('[class="jss130"]').should('be.visible');// Title "Website Scanner"
         cy.get('[class="MuiButton-label jss149"]').should('be.visible');//Button "Create a new scan"
-        cy.get('#root > div > main > div > div > div.MuiBox-root.jss141.empty-overlay-container.jss136 > div > div > div.MuiBox-root.jss155.jss116 > section > div.container-fluid > div.MuiBox-root.jss204.container.jss164 > div').should('be.visible');//List of pages reports
+        cy.get('[class="MuiBox-root jss205 jss170"]').should('be.visible');//List of pages reports
         cy.get('[class="MuiButton-label"]').should('be.visible').click();//User - Logout
         cy.get('div >div>ul>li').should('be.visible');//User - Logout
         cy.get('#user-menu > div:nth-child(1)').click();//User - Logout
