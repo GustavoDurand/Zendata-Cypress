047bad476b454095501f3386690e3084ccc6c01f
Gustavo Durand <gustavodurandd@gmail.com>
2023-09-07 11:34:44 -0300

    commit

--- cypress/e2e/Zendata-test-production/homepage_production_test.cy.js
+++ cypress/e2e/Zendata-test-production/homepage_production_test.cy.js
@@ -59,11 +59,11 @@ describe('Dashboard Test', function(){
         cy.get('.intercom-lightweight-app-launcher-icon-open').should('be.visible')//Chat boot
         cy.get('.complete').should('exist')//Final page of the report
     })
-    it('Demo Reports --Websites scans-- NVidia', () => {
+    it('Demo Reports --Websites scans--LetsDeel', () => {
         cy.visit('https://www.zendata.dev/');
         cy.get('body > div.home-header > div.navbar.w-nav > div.navigation-wrap > div > div:nth-child(1) > div > div.dropdown-content.product-dropdown.neo-dropdown > a.navigation-item.neo-navigation-item.mega-menu-item.ws-entry').click().should('be.visible')
-        cy.get(':nth-child(2) > .link-block-2 > .div-block-16 > .text-block-8').should('be.visible')//Button see results
-        cy.visit('https://app.zendata.dev/privacy-report?r=49059&c=web').should('exist')//Report of demo NVidia
+        cy.get(':nth-child(3) > .link-block-2 > .div-block-16 > .text-block-8').should('be.visible')//Button see results
+        cy.visit('https://app.zendata.dev/privacy-report?r=48871&c=web').should('exist')//Report of demo LetsDeel
         cy.get('.pdf-button-container > .MuiButtonBase-root > .MuiButton-label').should('exist')//Download button pdf report
         cy.get('#weglot-language-en').should('exist')//Change Language
         cy.get('.intercom-lightweight-app-launcher-icon-open').should('be.visible')//Chat boot
