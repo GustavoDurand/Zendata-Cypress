0a1136255872ddf09c0462fd40f73442e0d9a35d
Gustavo Durand <gustavodurandd@gmail.com>
2023-09-14 17:43:50 -0300

    commit

--- cypress/e2e/Zendata-test-production/homepage_production_test.spec.cy.js
+++ cypress/e2e/Zendata-test-production/homepage_production_test.spec.cy.js
@@ -1,6 +1,6 @@
-describe('Dashboard Test', function(){
+describe('Homepage Test', function(){
 
-    it('HomePage Test', () => {
+    it(' Home Principal', () => {
         cy.visit('https://www.zendata.dev/');
         cy.get('.intercom-lightweight-app-launcher-icon-open').should('be.visible')//Chatboot
         cy.get('.cb-button').should('be.visible')//cookie button
@@ -33,48 +33,6 @@ describe('Dashboard Test', function(){
         cy.get('.monitors-section').should('be.visible')
         cy.get('.scan-features-section > ._1200-container').should('be.visible')
 
-    //Demo Section
-        cy.get('#Demos > ._1200-container').should('be.visible').should('be.visible')
-        cy.get(':nth-child(1) > .link-block-2 > .div-block-16 > .text-block-8').should('be.visible')
-        cy.get(':nth-child(1) > .link-block-2 > .div-block-16 > .text-block-8').should('be.visible')
-        cy.get(':nth-child(1) > .link-block-2 > .div-block-16 > .text-block-8').should('be.visible')
-    })
-    it('Demo Reports --Websites scans-- NY Times', () => {
-        cy.visit('https://www.zendata.dev/');
-        cy.wait(10000)
-        cy.get('body > div.home-header > div.navbar.w-nav > div.navigation-wrap > div > div:nth-child(1) > div > div.dropdown-content.product-dropdown.neo-dropdown > a.navigation-item.neo-navigation-item.mega-menu-item.ws-entry').click().should('be.visible')
-        cy.get(':nth-child(1) > .link-block-2 > .div-block-16 > .text-block-8').should('be.visible')//Button see results
-        cy.visit('https://app.zendata.dev/privacy-report?r=49058&c=web').should('exist')//Report of demo NY Times
-        cy.wait(5000)
-        cy.get('.pdf-button-container > .MuiButtonBase-root > .MuiButton-label').should('exist')//Download button pdf report
-        cy.get('#weglot-language-en').should('exist')//Change Language
-        cy.get('.intercom-lightweight-app-launcher-icon-open').should('be.visible')//Chat boot
-        cy.get('.complete').should('exist')//Final page of the report
-
-    })
-    it('Demo Reports --Websites scans-- NVidia', () => {
-        cy.visit('https://www.zendata.dev/');
-        cy.wait(10000)
-        cy.get('body > div.home-header > div.navbar.w-nav > div.navigation-wrap > div > div:nth-child(1) > div > div.dropdown-content.product-dropdown.neo-dropdown > a.navigation-item.neo-navigation-item.mega-menu-item.ws-entry').click().should('be.visible')
-        cy.get(':nth-child(2) > .link-block-2 > .div-block-16 > .text-block-8').should('be.visible')//Button see results
-        cy.visit('https://app.zendata.dev/privacy-report?r=49059&c=web').should('exist')//Report of demo NVidia
-        cy.wait(5000)
-        cy.get('.pdf-button-container > .MuiButtonBase-root > .MuiButton-label').should('exist')//Download button pdf report
-        cy.get('#weglot-language-en').should('exist')//Change Language
-        cy.get('.intercom-lightweight-app-launcher-icon-open').should('be.visible')//Chat boot
-        cy.get('.complete').should('exist')//Final page of the report
-    })
-    it('Demo Reports --Websites scans--LetsDeel', () => {
-        cy.visit('https://www.zendata.dev/');
-        cy.wait(10000)
-        cy.get('body > div.home-header > div.navbar.w-nav > div.navigation-wrap > div > div:nth-child(1) > div > div.dropdown-content.product-dropdown.neo-dropdown > a.navigation-item.neo-navigation-item.mega-menu-item.ws-entry').click().should('be.visible')
-        cy.get(':nth-child(3) > .link-block-2 > .div-block-16 > .text-block-8').should('be.visible')//Button see results
-        cy.visit('https://app.zendata.dev/privacy-report?r=48871&c=web').should('exist')//Report of demo LetsDeel
-        cy.wait(5000)
-        cy.get('.pdf-button-container > .MuiButtonBase-root > .MuiButton-label').should('exist')//Download button pdf report
-        cy.get('#weglot-language-en').should('exist')//Change Language
-        cy.get('.intercom-lightweight-app-launcher-icon-open').should('be.visible')//Chat boot
-        cy.get('.complete').should('exist')//Final page of the report
     })
 
 
