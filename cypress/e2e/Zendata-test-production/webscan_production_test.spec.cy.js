c3bf41479b78976cb929555be8544b07d71d03ec
Gustavo Durand <gustavodurandd@gmail.com>
2023-09-14 18:39:57 -0300

    force click webscan

--- cypress/e2e/Zendata-test-production/webscan_production_test.spec.cy.js
+++ cypress/e2e/Zendata-test-production/webscan_production_test.spec.cy.js
@@ -6,8 +6,8 @@ describe('Website Scan Test', function(){
     })
 
     it('Create a New Scan',() =>{
-        cy.wait(8000)
-        cy.get('.jss147 > .MuiButtonBase-root > .MuiButton-label').click();//Create a new scan button
+
+        cy.get('.jss147 > .MuiButtonBase-root > .MuiButton-label').click({force: true});//Create a new scan button
         cy.get('.MuiTypography-h2').should('exist'); //Title Website Scanner
         cy.get('.jss260 > .MuiBox-root > :nth-child(2)').should('be.visible');// text #1
         cy.get('.jss260 > .MuiBox-root > :nth-child(3)').should('be.visible');// text #2
@@ -17,13 +17,13 @@ describe('Website Scan Test', function(){
         cy.get('.jss230 > .MuiFormControl-root > .MuiInputBase-root > .MuiInputBase-input').type('https://www.zendata.dev/').should('be.visible');//Type the URL to Scan
         cy.get('.jss259 > :nth-child(4)').should('exist');//search sub pages button
         cy.get(':nth-child(5) > .jss231 > .MuiTypography-root').should('exist');//repeats text
-        cy.get('.MuiSelect-root').click().should('be.visible');//List of times
-        cy.get('.Mui-selected').click().should('be.visible');//Select never
+        cy.get('.MuiSelect-root').click({force: true}).should('be.visible');//List of times
+        cy.get('.Mui-selected').click({force: true}).should('be.visible');//Select never
         cy.get('.-with-margin > .jss231 > .MuiTypography-root').should('exist');//text countries
         cy.get('.MuiAutocomplete-endAdornment > .MuiButtonBase-root > .MuiIconButton-label > .MuiSvgIcon-root').click().should('be.visible');//List of countries
-        cy.contains('United States').click();//select a countrie
+        cy.contains('United States').click({force: true});//select a countrie
         cy.get('.jss288 > .MuiButtonBase-root > .MuiButton-label').should('be.visible');//button send scan
         cy.get('.jss151 > .MuiButton-label').should('exist').should('be.visible');
-        cy.get('.jss288 > .MuiButtonBase-root > .MuiButton-label').click();// send scan
+        cy.get('.jss288 > .MuiButtonBase-root > .MuiButton-label').click({force: true});// send scan
     });
 })
