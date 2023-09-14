54e1e1739eb3ea47dc9cb177a7bb6bb981f6192b
Gustavo Durand <gustavodurandd@gmail.com>
2023-09-14 18:23:11 -0300

    fix code demo reports

--- cypress/e2e/Zendata-test-production/demo_reports_test.spec.cy.js
+++ cypress/e2e/Zendata-test-production/demo_reports_test.spec.cy.js
@@ -1,15 +1,15 @@
-describe('Dashboard Test', function(){
+describe('Demo Reports Test', function(){
 
     beforeEach(function(){
-        cy.visit('https://www.zendata.dev/'); // comando
+        cy.visit('https://www.zendata.dev/website-scanner'); // comando
     })
 
 
 //Demo Section
 it('Demo Reports --Websites scans-- NY Times', () => {
-    
-cy.get('body > div.home-header > div.navbar.w-nav > div.navigation-wrap > div > div:nth-child(1) > div > div.dropdown-content.product-dropdown.neo-dropdown > a.navigation-item.neo-navigation-item.mega-menu-item.ws-entry').click().should('be.visible')
-cy.get(':nth-child(1) > .link-block-2 > .div-block-16 > .text-block-8').should('be.visible').click()//Button see results
+
+cy.get(':nth-child(1) > .link-block-2 > .div-block-16 > .text-block-8').should('be.visible')//Button see results
+cy.visit('https://app.zendata.dev/privacy-report?r=49058&c=web')
 cy.wait(5000)
 cy.get('.pdf-button-container > .MuiButtonBase-root > .MuiButton-label').should('exist')//Download button pdf report
 cy.get('.complete').should('exist')//Final page of the report
@@ -17,8 +17,8 @@ cy.get('.complete').should('exist')//Final page of the report
 
 it('Demo Reports --Websites scans-- NVidia', () => {
 
-cy.get('body > div.home-header > div.navbar.w-nav > div.navigation-wrap > div > div:nth-child(1) > div > div.dropdown-content.product-dropdown.neo-dropdown > a.navigation-item.neo-navigation-item.mega-menu-item.ws-entry').click().should('be.visible')
-cy.get(':nth-child(2) > .link-block-2 > .div-block-16 > .text-block-8').should('be.visible').click()//Button see results
+cy.get(':nth-child(2) > .link-block-2 > .div-block-16 > .text-block-8').should('be.visible')//Button see results
+cy.visit('https://app.zendata.dev/privacy-report?r=49059&c=web')
 cy.wait(5000)
 cy.get('.pdf-button-container > .MuiButtonBase-root > .MuiButton-label').should('exist')//Download button pdf report
 cy.get('.complete').should('exist')//Final page of the report
@@ -26,8 +26,8 @@ cy.get('.complete').should('exist')//Final page of the report
 
 it('Demo Reports --Websites scans--LetsDeel', () => {
 
-cy.get('body > div.home-header > div.navbar.w-nav > div.navigation-wrap > div > div:nth-child(1) > div > div.dropdown-content.product-dropdown.neo-dropdown > a.navigation-item.neo-navigation-item.mega-menu-item.ws-entry').click().should('be.visible')
-cy.get(':nth-child(3) > .link-block-2 > .div-block-16 > .text-block-8').should('be.visible').click()//Button see results
+cy.get(':nth-child(3) > .link-block-2 > .div-block-16 > .text-block-8').should('be.visible')//Button see results
+cy.visit('https://app.zendata.dev/privacy-report?r=48871&c=web')
 cy.wait(5000)
 cy.get('.pdf-button-container > .MuiButtonBase-root > .MuiButton-label').should('exist')//Download button pdf report
 cy.get('.complete').should('exist')//Final page of the report
